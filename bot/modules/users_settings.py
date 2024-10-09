noobzhax/mltb

Add thumbnail layout option
Signed-off-by: anasty17 <e.anastayyar@gmail.com>

bot/modules/users_settings.py
@@ -81,15 +81,15 @@ async def get_user_settings(from_user):
 
     if user_dict.get("lprefix", False):
         lprefix = user_dict["lprefix"]
-    elif "lprefix" not in user_dict and (LP := config_dict["LEECH_FILENAME_PREFIX"]):
-        lprefix = LP
+    elif "lprefix" not in user_dict and config_dict["LEECH_FILENAME_PREFIX"]:
+        lprefix = config_dict["LEECH_FILENAME_PREFIX"]
     else:
         lprefix = "None"
 
     if user_dict.get("leech_dest", False):
         leech_dest = user_dict["leech_dest"]
-    elif "leech_dest" not in user_dict and (LD := config_dict["LEECH_DUMP_CHAT"]):
-        leech_dest = LD
+    elif "leech_dest" not in user_dict and config_dict["LEECH_DUMP_CHAT"]:
+        leech_dest = config_dict["LEECH_DUMP_CHAT"]
     else:
         leech_dest = "None"
 
@@ -112,6 +112,13 @@ async def get_user_settings(from_user):
         mixed_leech = "Enabled"
     else:
         mixed_leech = "Disabled"
+
+    if user_dict.get("thumb_layout", False):
+        thumb_layout = user_dict["thumb_layout"]
+    elif "thumb_layout" not in user_dict and config_dict["THUMBNAIL_LAYOUT"]:
+        thumb_layout = config_dict["THUMBNAIL_LAYOUT"]
+    else:
+        thumb_layout = "None"
 
     buttons.data_button("Leech", f"userset {user_id} leech")
 
@@ -186,6 +193,7 @@ Leech Prefix is <code>{escape(lprefix)}</code>
 Leech Destination is <code>{leech_dest}</code>
 Leech by <b>{leech_method}</b> session
 Mixed Leech is <b>{mixed_leech}</b>
+Thumbnail Layout is <b>{thumb_layout}</b>
 Rclone Config <b>{rccmsg}</b>
 Rclone Path is <code>{rccpath}</code>
 Gdrive Token <b>{tokenmsg}</b>
@@ -387,6 +395,7 @@ async def edit_user_settings(client, query):
         "index_url",
         "excluded_extensions",
         "name_sub",
+        "thumb_layout",
     ]:
         await query.answer()
         update_user_ldata(user_id, data[2], "")
@@ -414,17 +423,15 @@ async def edit_user_settings(client, query):
         buttons.data_button("Leech Destination", f"userset {user_id} ldest")
         if user_dict.get("leech_dest", False):
             leech_dest = user_dict["leech_dest"]
-        elif "leech_dest" not in user_dict and (LD := config_dict["LEECH_DUMP_CHAT"]):
-            leech_dest = LD
+        elif "leech_dest" not in user_dict and config_dict["LEECH_DUMP_CHAT"]:
+            leech_dest = config_dict["LEECH_DUMP_CHAT"]
         else:
             leech_dest = "None"
         buttons.data_button("Leech Prefix", f"userset {user_id} leech_prefix")
         if user_dict.get("lprefix", False):
             lprefix = user_dict["lprefix"]
-        elif "lprefix" not in user_dict and (
-            LP := config_dict["LEECH_FILENAME_PREFIX"]
-        ):
-            lprefix = LP
+        elif "lprefix" not in user_dict and config_dict["LEECH_FILENAME_PREFIX"]:
+            lprefix = config_dict["LEECH_FILENAME_PREFIX"]
         else:
             lprefix = "None"
         if (
@@ -501,6 +508,14 @@ async def edit_user_settings(client, query):
         else:
             mixed_leech = "Disabled"
 
+        buttons.data_button("Thumbnail Layout", f"userset {user_id} tlayout")
+        if user_dict.get("thumb_layout", False):
+            thumb_layout = user_dict["thumb_layout"]
+        elif "thumb_layout" not in user_dict and config_dict["THUMBNAIL_LAYOUT"]:
+            thumb_layout = config_dict["THUMBNAIL_LAYOUT"]
+        else:
+            thumb_layout = "None"
+
         buttons.data_button("Back", f"userset {user_id} back")
         buttons.data_button("Close", f"userset {user_id} close")
         text = f"""<u>Leech Settings for {name}</u>
@@ -513,6 +528,7 @@ Leech Prefix is <code>{escape(lprefix)}</code>
 Leech Destination is <code>{leech_dest}</code>
 Leech by <b>{leech_method}</b> session
 Mixed Leech is <b>{mixed_leech}</b>
+Thumbnail Layout is <b>{thumb_layout}</b>
 """
         await edit_message(message, text, buttons.build_menu(2))
     elif data[2] == "rclone":
@@ -718,6 +734,26 @@ Check all yt-dlp api options from this <a href='https://github.com/yt-dlp/yt-dlp
             buttons.build_menu(1),
         )
         pfunc = partial(set_option, pre_event=query, option="leech_dest")
+        await event_handler(client, query, pfunc)
+    elif data[2] == "tlayout":
+        await query.answer()
+        buttons = ButtonMaker()
+        if (
+            user_dict.get("thumb_layout", False)
+            or "thumb_layout" not in user_dict
+            and config_dict["THUMBNAIL_LAYOUT"]
+        ):
+            buttons.data_button(
+                "Reset Thumbnail Layout", f"userset {user_id} thumb_layout"
+            )
+        buttons.data_button("Back", f"userset {user_id} leech")
+        buttons.data_button("Close", f"userset {user_id} close")
+        await edit_message(
+            message,
+            "Send thumbnail layout (widthxheight, 2x2, 3x3, 2x4, 4x4, ...). Timeout: 60 sec",
+            buttons.build_menu(1),
+        )
+        pfunc = partial(set_option, pre_event=query, option="thumb_layout")
         await event_handler(client, query, pfunc)
     elif data[2] == "ex_ex":
         await query.answer()

radinkaputri/anashk

bot/modules/users_settings.py
@@ -78,15 +78,15 @@ async def get_user_settings(from_user):
 
     if user_dict.get("lprefix", False):
         lprefix = user_dict["lprefix"]
-    elif "lprefix" not in user_dict and (LP := config_dict["LEECH_FILENAME_PREFIX"]):
-        lprefix = LP
+    elif "lprefix" not in user_dict and config_dict["LEECH_FILENAME_PREFIX"]:
+        lprefix = config_dict["LEECH_FILENAME_PREFIX"]
     else:
         lprefix = "None"
 
     if user_dict.get("leech_dest", False):
         leech_dest = user_dict["leech_dest"]
-    elif "leech_dest" not in user_dict and (LD := config_dict["LEECH_DUMP_CHAT"]):
-        leech_dest = LD
+    elif "leech_dest" not in user_dict and config_dict["LEECH_DUMP_CHAT"]:
+        leech_dest = config_dict["LEECH_DUMP_CHAT"]
     else:
         leech_dest = "None"
 
@@ -109,6 +109,12 @@ async def get_user_settings(from_user):
         mixed_leech = "Enabled"
     else:
         mixed_leech = "Disabled"
+    if user_dict.get("thumb_layout", False):
+        thumb_layout = user_dict["thumb_layout"]
+    elif "thumb_layout" not in user_dict and config_dict["THUMBNAIL_LAYOUT"]:
+        thumb_layout = config_dict["THUMBNAIL_LAYOUT"]
+    else:
+        thumb_layout = "None"
 
     buttons.ibutton("Leech", f"userset {user_id} leech")
 
@@ -183,6 +189,7 @@ Leech Prefix is <code>{escape(lprefix)}</code>
 Leech Destination is <code>{leech_dest}</code>
 Leech by <b>{leech_method}</b> session
 Mixed Leech is <b>{mixed_leech}</b>
+Thumbnail Layout is <b>{thumb_layout}</b>
 Rclone Config <b>{rccmsg}</b>
 Rclone Path is <code>{rccpath}</code>
 Gdrive Token <b>{tokenmsg}</b>
@@ -381,6 +388,7 @@ async def edit_user_settings(client, query):
         "index_url",
         "excluded_extensions",
         "name_sub",
+        "thumb_layout",
     ]:
         await query.answer()
         update_user_ldata(user_id, data[2], "")
@@ -408,17 +416,15 @@ async def edit_user_settings(client, query):
         buttons.ibutton("Leech Destination", f"userset {user_id} ldest")
         if user_dict.get("leech_dest", False):
             leech_dest = user_dict["leech_dest"]
-        elif "leech_dest" not in user_dict and (LD := config_dict["LEECH_DUMP_CHAT"]):
-            leech_dest = LD
+        elif "leech_dest" not in user_dict and config_dict["LEECH_DUMP_CHAT"]:
+            leech_dest = config_dict["LEECH_DUMP_CHAT"]
         else:
             leech_dest = "None"
         buttons.ibutton("Leech Prefix", f"userset {user_id} leech_prefix")
         if user_dict.get("lprefix", False):
             lprefix = user_dict["lprefix"]
-        elif "lprefix" not in user_dict and (
-            LP := config_dict["LEECH_FILENAME_PREFIX"]
-        ):
-            lprefix = LP
+        elif "lprefix" not in user_dict and config_dict["LEECH_FILENAME_PREFIX"]:
+            lprefix = config_dict["LEECH_FILENAME_PREFIX"]
         else:
             lprefix = "None"
         if (
@@ -491,6 +497,14 @@ async def edit_user_settings(client, query):
         else:
             mixed_leech = "Disabled"
 
+        buttons.ibutton("Thumbnail Layout", f"userset {user_id} tlayout")
+        if user_dict.get("thumb_layout", False):
+            thumb_layout = user_dict["thumb_layout"]
+        elif "thumb_layout" not in user_dict and config_dict["THUMBNAIL_LAYOUT"]:
+            thumb_layout = config_dict["THUMBNAIL_LAYOUT"]
+        else:
+            thumb_layout = "None"
+
         buttons.ibutton("Back", f"userset {user_id} back")
         buttons.ibutton("Close", f"userset {user_id} close")
         text = f"""<u>Leech Settings for {name}</u>
@@ -503,6 +517,7 @@ Leech Prefix is <code>{escape(lprefix)}</code>
 Leech Destination is <code>{leech_dest}</code>
 Leech by <b>{leech_method}</b> session
 Mixed Leech is <b>{mixed_leech}</b>
+Thumbnail Layout is <b>{thumb_layout}</b>
 """
         await editMessage(message, text, buttons.build_menu(2))
     elif data[2] == "rclone":
@@ -702,6 +717,26 @@ Check all yt-dlp api options from this <a href='https://github.com/yt-dlp/yt-dlp
             buttons.build_menu(1),
         )
         pfunc = partial(set_option, pre_event=query, option="leech_dest")
+        await event_handler(client, query, pfunc)
+    elif data[2] == "tlayout":
+        await query.answer()
+        buttons = ButtonMaker()
+        if (
+            user_dict.get("thumb_layout", False)
+            or "thumb_layout" not in user_dict
+            and config_dict["THUMBNAIL_LAYOUT"]
+        ):
+            buttons.data_button(
+                "Reset Thumbnail Layout", f"userset {user_id} thumb_layout"
+            )
+        buttons.data_button("Back", f"userset {user_id} leech")
+        buttons.data_button("Close", f"userset {user_id} close")
+        await edit_message(
+            message,
+            "Send thumbnail layout (widthxheight, 2x2, 3x3, 2x4, 4x4, ...). Timeout: 60 sec",
+            buttons.build_menu(1),
+        )
+        pfunc = partial(set_option, pre_event=query, option="thumb_layout")
         await event_handler(client, query, pfunc)
     elif data[2] == "ex_ex":
         await query.answer()