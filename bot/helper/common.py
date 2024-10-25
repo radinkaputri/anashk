noobzhax/mltb

Add -doc and -med args for leech type to use them as task option - Force thumbnail layout on yt-dlp videos
Signed-off-by: anasty17 <e.anastayyar@gmail.com>

bot/helper/common.py
@@ -111,12 +111,13 @@ class TaskConfig:
         self.convert_audio = False
         self.convert_video = False
         self.screen_shots = False
-        self.as_doc = False
         self.is_cancelled = False
         self.force_run = False
         self.force_download = False
         self.force_upload = False
         self.is_torrent = False
+        self.as_med = False
+        self.as_doc = False
         self.chat_thread_id = None
         self.suproc = None
         self.thumb = None
@@ -357,11 +358,12 @@ class TaskConfig:
             )
             self.split_size = min(self.split_size, self.max_split_size)
 
-            self.as_doc = (
-                self.user_dict.get("as_doc", False)
-                or config_dict["AS_DOCUMENT"]
-                and "as_doc" not in self.user_dict
-            )
+            if not self.as_doc:
+                self.as_doc = not self.as_med if self.as_med else (
+                    self.user_dict.get("as_doc", False)
+                    or config_dict["AS_DOCUMENT"]
+                    and "as_doc" not in self.user_dict
+                )
 
             self.thumbnail_layout = (
                 self.thumbnail_layout

bot/helper/mirror_leech_utils/download_utils/yt_dlp_download.py
@@ -274,7 +274,7 @@ class YoutubeDLHelper:
         if qual.startswith("ba/b"):
             self._listener.name = f"{base_name}{self._ext}"
 
-        if self._listener.is_leech:
+        if self._listener.is_leech and not self._listener.thumbnail_layout:
             self.opts["postprocessors"].append(
                 {
                     "format": "jpg",
@@ -296,7 +296,9 @@ class YoutubeDLHelper:
         ]:
             self.opts["postprocessors"].append(
                 {
-                    "already_have_thumbnail": self._listener.is_leech,
+                    "already_have_thumbnail": bool(
+                        self._listener.is_leech and not self._listener.thumbnail_layout
+                    ),
                     "key": "EmbedThumbnail",
                 }
             )

radinkaputri/anashk

bot/helper/common.py
@@ -115,6 +115,8 @@ class TaskConfig:
         self.forceDownload = False
         self.forceUpload = False
         self.isTorrent = False
+        self.as_med = False
+        self.as_doc = False
         self.suproc = None
         self.thumb = None
         self.time = ""
@@ -331,11 +333,12 @@ class TaskConfig:
             self.maxSplitSize = MAX_SPLIT_SIZE if self.userTransmission else 2097152000
             self.splitSize = min(self.splitSize, self.maxSplitSize)
 
-            self.asDoc = (
-                self.userDict.get("as_doc", False)
-                or config_dict["AS_DOCUMENT"]
-                and "as_doc" not in self.userDict
-            )
+            if not self.as_doc:
+                self.as_doc = not self.as_med if self.as_med else (
+                    self.userDict.get("as_doc", False)
+                    or config_dict["AS_DOCUMENT"]
+                    and "as_doc" not in self.userDict
+                )
 
             self.thumbnail_layout = (
                 self.thumbnail_layout

bot/helper/mirror_leech_utils/download_utils/yt_dlp_download.py
@@ -277,7 +277,7 @@ class YoutubeDLHelper:
         if qual.startswith("ba/b"):
             self._listener.name = f"{base_name}{self._ext}"
 
-        if self._listener.isLeech:
+        if self._listener.isLeech and not self._listener.thumbnail_layout:
             self.opts["postprocessors"].append(
                 {
                     "format": "jpg",
@@ -299,7 +299,10 @@ class YoutubeDLHelper:
         ]:
             self.opts["postprocessors"].append(
                 {
-                    "already_have_thumbnail": self._listener.isLeech,
+
+                    "already_have_thumbnail": bool(
+                        self._listener.isLeech and not self._listener.thumbnail_layout
+                    ),
                     "key": "EmbedThumbnail",
                 }
             )