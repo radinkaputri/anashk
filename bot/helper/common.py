noobzhax/mltb

Fix leech upload des while using username and topics - added topic support for rss
Signed-off-by: anasty17 <e.anastayyar@gmail.com>

bot/helper/common.py
@@ -177,7 +177,11 @@ class TaskConfig:
             else ["aria2", "!qB"]
         )
         if self.link not in ["rcl", "gdl"]:
-            if not self.is_jd and is_rclone_path(self.link) or is_gdrive_link(self.link):
+            if (
+                not self.is_jd
+                and is_rclone_path(self.link)
+                or is_gdrive_link(self.link)
+            ):
                 await self.is_token_exists(self.link, "dl")
         elif self.link == "rcl":
             if not self.is_ytdlp and not self.is_jd:
@@ -290,9 +294,12 @@ class TaskConfig:
                         self.mixed_leech = self.user_transmission
                     if "|" in self.up_dest:
                         self.up_dest, self.chat_thread_id = list(
-                            map(int, self.up_dest.split("|", 1))
+                            map(
+                                lambda x: int(x) if x.lstrip("-").isdigit() else x,
+                                self.up_dest.split("|", 1),
+                            )
                         )
-                    elif self.up_dest.isdigit() or self.up_dest.startswith("-"):
+                    elif self.up_dest.lstrip("-").isdigit():
                         self.up_dest = int(self.up_dest)
                     elif self.up_dest.lower() == "pm":
                         self.up_dest = self.user_id

bot/modules/rss.py
@@ -644,7 +644,8 @@ Timeout: 60 sec. Argument -c for command and arguments
 
 
 async def rss_monitor():
-    if not config_dict["RSS_CHAT"]:
+    chat = config_dict["RSS_CHAT"]
+    if not chat:
         LOGGER.warning("RSS_CHAT not added! Shutting down rss scheduler...")
         scheduler.shutdown(wait=False)
         return
@@ -652,6 +653,18 @@ async def rss_monitor():
         scheduler.pause()
         return
     all_paused = True
+    rss_topic_id = rss_chat_id = None
+    if isinstance(chat, int):
+        rss_chat_id = chat
+    elif "|" in chat:
+        rss_chat_id, rss_topic_id = list(
+            map(
+                lambda x: int(x) if x.lstrip("-").isdigit() else x,
+                chat.split("|", 1),
+            )
+        )
+    elif chat.lstrip("-").isdigit():
+        rss_chat_id = int(chat)
     for user, items in list(rss_dict.items()):
         for title, data in items.items():
             try:
@@ -737,7 +750,7 @@ async def rss_monitor():
                     feed_msg += (
                         f"\n<b>Tag: </b><code>{data['tag']}</code> <code>{user}</code>"
                     )
-                    await send_rss(feed_msg)
+                    await send_rss(feed_msg, rss_chat_id, rss_topic_id)
                     feed_count += 1
                 async with rss_dict_lock:
                     if user not in rss_dict or not rss_dict[user].get(title, False):

radinkaputri/anashk

bot/helper/common.py
@@ -283,9 +283,12 @@ class TaskConfig:
                         self.mixedLeech = self.userTransmission
                     if "|" in self.upDest:
                         self.upDest, self.chat_thread_id = list(
-                            map(int, self.upDest.split("|", 1))
+                            map(
+                                lambda x: int(x) if x.lstrip("-").isdigit() else x,
+                                self.upDest.split("|", 1),
+                            )
                         )
-                    elif self.upDest.isdigit() or self.upDest.startswith("-"):
+                    elif self.upDest.lstrip("-").isdigit():
                         self.upDest = int(self.upDest)
                     elif self.upDest.lower() == "pm":
                         self.upDest = self.userId

bot/modules/rss.py
@@ -647,7 +647,8 @@ Timeout: 60 sec. Argument -c for command and arguments
 
 
 async def rssMonitor():
-    if not config_dict["RSS_CHAT"]:
+    chat = config_dict["RSS_CHAT"]
+    if not chat:
         LOGGER.warning("RSS_CHAT not added! Shutting down rss scheduler...")
         scheduler.shutdown(wait=False)
         return
@@ -655,6 +656,18 @@ async def rssMonitor():
         scheduler.pause()
         return
     all_paused = True
+    rss_topic_id = rss_chat_id = None
+    if isinstance(chat, int):
+        rss_chat_id = chat
+    elif "|" in chat:
+        rss_chat_id, rss_topic_id = list(
+            map(
+                lambda x: int(x) if x.lstrip("-").isdigit() else x,
+                chat.split("|", 1),
+            )
+        )
+    elif chat.lstrip("-").isdigit():
+        rss_chat_id = int(chat)
     for user, items in list(rss_dict.items()):
         for title, data in items.items():
             try:
@@ -745,7 +758,7 @@ async def rssMonitor():
                     feed_msg += (
                         f"\n<b>Tag: </b><code>{data['tag']}</code> <code>{user}</code>"
                     )
-                    await sendRss(feed_msg)
+                    await sendRss(feed_msg, rss_chat_id, rss_topic_id)
                     feed_count += 1
                 async with rss_dict_lock:
                     if user not in rss_dict or not rss_dict[user].get(title, False):